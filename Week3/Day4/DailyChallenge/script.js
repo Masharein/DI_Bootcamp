1ff8c78871183500f18781884737cce49bbb8e1a
Masharein <Masharein04061995@gmail.com>
2023-02-01 15:13:28 +0300

    new

--- Week3/Day4/DailyChallenge/script.js
+++ Week3/Day4/DailyChallenge/script.js
@@ -1,16 +1,27 @@
 const form = document.getElementById("MyForm");
 const list = document.querySelector(".listTasks");
-
+const tasks = [];
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const input = document.getElementById("text");
   const task = input.value;
   if (!task) {
     return;
-  }
-  const newTask = document.createElement("div");
-  newTask.classList.add("listStyle")
-  newTask.innerHTML = task;
-  list.appendChild(newTask);
-  input.value = "";
+  } else {
+    tasks.push(task);
+    console.log(tasks)
+} 
+const newTask = document.createElement("div");
+newTask.classList.add("listStyle");
+const checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+const label = document.createElement("label");
+label.innerHTML = task;
+newTask.appendChild(checkbox);
+newTask.appendChild(label);
+const removeButton = document.createElement("i");
+removeButton.classList.add("fa-solid", "fa-square-xmark");
+newTask.appendChild(removeButton);
+list.appendChild(newTask);
+input.value = "";
 });
