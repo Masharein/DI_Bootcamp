f4d6fc3246bae0830bd0ce11db8f8a529500be9f
Masharein <Masharein04061995@gmail.com>
2023-02-08 10:39:29 +0300

    new

--- Week4/Day3/DailyChallenge/script.js
+++ Week4/Day3/DailyChallenge/script.js
@@ -15,9 +15,16 @@ const inventory = [
     { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
   ];
 
-    const carInventory = inventory.find(car => car.car_make === "Honda")
-    console.log(carInventory)
-    console.log(`This is a ${carInventory.car_make} ${carInventory.car_model} from ${carInventory.car_year}`)
+  function getCarHonda(carInventory) {
+    const honda = carInventory.find(car => car.car_make === "Honda");
+    if (honda) {
+      return `This is a ${honda.car_make} ${honda.car_model} from ${honda.car_year}`
+    } else {
+      return "No Honda car found in the inventory";
+    }
+  }
+  console.log(getCarHonda(inventory));
+
 
     // Part II
 
@@ -32,18 +39,11 @@ const inventory = [
         { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
         { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
         { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
-      ];
-        
-    //    function sortCarInventoryByYear(carInventory) {
-    //     const carInventory = inventory.map;
-    //     const sortedYears= carInventory.sort((a, b) => a.car_year - b.car_year);
-    //     return sortedYears
-    //    }
-    //    sortCarInventoryByYear(1985)
-
-
-
-        const sortedYears = inventory.sort((a, b) => a.car_year - b.car_year);
-        console.log(sortedYears)
-      
-
+    ];
+    
+    function sortCarInventoryByYear(carInventory) {
+        return carInventory.sort((a, b) => a.car_year - b.car_year);
+    }
+    
+    console.log(sortCarInventoryByYear(inventory));
+    
