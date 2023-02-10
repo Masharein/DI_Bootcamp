26085c3e73f3d9de4df84e524994a369a31fcf05
Masharein <Masharein04061995@gmail.com>
2023-02-10 12:19:36 +0300

    portfolio

---
+++ portfolio-online/script.js
@@ -0,0 +1,35 @@
+//typing animation//
+var typed = new Typed(".typing", {
+    strings:["Full-Stack Developer", "Front-End Developer", "Back-End Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+//aside//
+const nav = document.querySelector(".nav"),
+navList = nav.querySelectorAll("li"),
+totalNavList = navList.length,
+allSection = document.querySelectorAll(".section"),
+totalSection = allSection.length;
+for(let i=0; i<totalNavList; i++)
+{
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function() {
+        for(let j=0; j<totalNavList; j++)
+        {
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+        showSection(this);
+    })
+}
+function showSection(element)
+{
+    for(let i=0; i<totalSection; i++)
+    {
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active")
+}
