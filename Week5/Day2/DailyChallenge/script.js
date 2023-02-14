65fedcf03526f44e2900e356022009fabcf98d25
Masharein <Masharein04061995@gmail.com>
2023-02-14 11:07:20 +0300

    new

--- Week5/Day2/DailyChallenge/script.js
+++ Week5/Day2/DailyChallenge/script.js
@@ -5,59 +5,50 @@
 // The gif should be appended with a DELETE button next to it. Hint : to find the URL of the gif, look for the sub-object named “images” inside the data you receive from the API.
 // Allow the user to delete a specific gif by clicking the DELETE button.
 // Allow the user to remove all of the GIFs by clicking a DELETE ALL button.
-let theForm = document.forms.theForm;
-console.log(theForm);
+// let theForm = document.forms.theForm;
+// console.log(theForm);
+// theForm.addEventListener("submit", checkTheValue);
+
+
+const theForm = document.forms.theForm;
 theForm.addEventListener("submit", checkTheValue);
 
 function checkTheValue(evt) {
   evt.preventDefault();
   const search = document.querySelector("#search").value;
-  console.log(search);
 
-  let xhr = new XMLHttpRequest();
-  xhr.open(
-    "GET",
-    `https://api.giphy.com/v1/gifs/search?q=${search}&rating=g&limit=1&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`,
-    true
-  );
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
+  // Send a GET request to the Giphy API
+  fetch(`https://api.giphy.com/v1/gifs/random?tag=${search}&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`)
+    .then(response => response.json())
+    .then(data => {
       displayGif(data);
-    }
-  };
-  xhr.send();
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
-function displayGif(data) {
-  const gifsContainer = document.querySelector("#gifsContainer")
-  const gifUrl = data.data[0].images.original.url;
+ function displayGif(data) {
+  const gifsContainer = document.querySelector("#gifsContainer");
+  const gifUrl = data.data.images.original.url;
   const gifElement = document.createElement("img");
   gifElement.src = gifUrl;
   const deleteBtn = document.createElement("button");
   deleteBtn.innerHTML = "Delete";
   deleteBtn.addEventListener("click", () => {
+    // Remove the GIF and the delete button when the delete button is clicked
     gifsContainer.removeChild(gifElement);
     gifsContainer.removeChild(deleteBtn);
   });
   const deleteAllBtn = document.createElement("button");
   deleteAllBtn.innerHTML = "Delete all";
   deleteAllBtn.addEventListener("click", () => {
-    gifsContainer.remove(gifElement);
-    gifsContainer.remove(deleteAllBtn);
+    // Remove all GIFs and buttons when the delete all button is clicked
+    gifsContainer.innerHTML = "";
   });
   gifsContainer.appendChild(gifElement);
   gifsContainer.appendChild(deleteBtn);
   gifsContainer.appendChild(deleteAllBtn);
 }
-
-deleteAllBtn.addEventListener("click", () => {
-  gifsContainer.innerHTML = "";
-});
-
-
-
-
-
 
 
--- Week5/Day3/script.js
+++ Week5/Day3/script.js
@@ -1,7 +1,4 @@
-fetch('ttps://jsonplaceholder.typicode.com/todos')
-.then(response => {
-    console.log(response)
-    return response.json()
-})
-.then(json => rconsole.log(json))
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(response => response.json())
+.then(json => console.log(json))
 .catch(error => console.error(error))
