cbd010bd18a0b49d1ccd3e7bdb3a770c918a3c85
Masharein <Masharein04061995@gmail.com>
2023-02-20 23:43:13 +0300

    NEW

--- Week6/Day2/ExercisesXP/first.sql
+++ Week6/Day2/ExercisesXP/first.sql
@@ -2,10 +2,11 @@
 SELECT * FROM items
 ORDER BY price_item ASC;
 -- Items with a price above 80 (80 included), ordered by price (highest to lowest).
-ORDER BY price_item DESC;
--- The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results.
+SELECT * FROM items
+ORDER BY price_item DESC WHERE price_item >= 80;
+-- -- The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results.
 SELECT * FROM customers
-ORDER BY first_name ASC;
--- All last names (no other columns!), in reverse alphabetical order (Z-A)
+ORDER BY first_name ASC LIMIT(3);
+-- -- All last names (no other columns!), in reverse alphabetical order (Z-A)
 SELECT last_name FROM customers
 ORDER BY last_name DESC;
