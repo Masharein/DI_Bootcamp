0797ce6eaad1f285d896d71d3e70a74213625dc1
Masharein <Masharein04061995@gmail.com>
2023-02-24 11:19:11 +0300

    new

--- Week6/Day5/task-list.js
+++ Week6/Day5/task-list.js
@@ -39,67 +39,6 @@ tasks.forEach((task) => {
       description.style.display = description.style.display === 'none' ? 'block' : 'none';
     });
   
-   // Add event listeners to edit button
-const editBtn = li.querySelector('.edit-btn');
-editBtn.addEventListener('click', () => {
-  // Show the edit task modal
-  const modal = document.getElementById('edit-modal');
-  modal.style.display = 'block';
-
-  // Pre-populate the input fields with the current task values
-  const nameInput = modal.querySelector('#edit-name');
-  const descriptionInput = modal.querySelector('#edit-description');
-  const startInput = modal.querySelector('#edit-start');
-  const endInput = modal.querySelector('#edit-end');
-
-  nameInput.value = task.name;
-  descriptionInput.value = task.description;
-  startInput.value = task.start;
-  endInput.value = task.end;
-
-  // Add event listener to the save button in the modal
-  const saveBtn = modal.querySelector('#edit-save');
-  saveBtn.addEventListener('click', () => {
-    // Get the new task values from the input fields
-    const newName = nameInput.value;
-    const newDescription = descriptionInput.value;
-    const newStart = startInput.value;
-    const newEnd = endInput.value;
-
-    // Update the task object with the new values
-    task.name = newName;
-    task.description = newDescription;
-    task.start = newStart;
-    task.end = newEnd;
-
-    // Save the updated task list in localStorage
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
-    // Update the task list element with the updated task values
-    li.querySelector('div').innerHTML = `
-      <input type="checkbox" ${task.isCompleted ? 'checked' : ''}>
-      Name: ${task.name}<br>
-      Description: ${task.description}<br>
-      Start date and time: ${task.start}<br>
-      End date and time: ${task.end}<br>
-      Days Left: ${daysLeft}
-      <button class="description-btn">Show/Hide Description</button>
-      <button class="edit-btn">Edit</button>
-      <button class="delete-btn">Delete</button>
-    `;
-
-    // Hide the edit task modal
-    modal.style.display = 'none';
-  });
-
-  // Add event listener to the cancel button in the modal
-  const cancelBtn = modal.querySelector('#edit-cancel');
-  cancelBtn.addEventListener('click', () => {
-    // Hide the edit task modal without making any changes
-    modal.style.display = 'none';
-  });
-});
-
     const deleteBtn = li.querySelector('.delete-btn');
     deleteBtn.addEventListener('click', () => {
       const confirmDelete = confirm('Are you sure you want to delete this task?');
@@ -109,5 +48,30 @@ editBtn.addEventListener('click', () => {
         li.remove();
       }
     });
+
+    const editBtn = li.querySelector('.edit-btn');
+    editBtn.addEventListener('click', function() {
+        const newName = prompt('Enter a new name:', task.name);
+        if (newName) {
+          task.name = newName;
+          const newDescription = prompt('Enter a new description:', task.description);
+          if (newDescription) {
+            task.description = newDescription;
+            const newStart = prompt('Enter a new start date and time (YYYY-MM-DDTHH:MM):', new Date(task.start).toISOString().slice(0, 16));
+            if (newStart) {
+              task.start = new Date(newStart).getTime();
+              const newEnd = prompt('Enter a new end date and time (YYYY-MM-DDTHH:MM):', new Date(task.end).toISOString().slice(0, 16));
+              if (newEnd) {
+                task.end = new Date(newEnd).getTime();
+                localStorage.setItem('tasks', JSON.stringify(tasks));
+                task.name.textContent =  `Name: ${task.name}<br>`
+                task.description.textContent = 'Description: ' + task.description;
+                task.start.textContent =  `Start date and time: ${task.start}<br>`;
+                task.end.textContent = ` End date and time: ${task.end}<br>`;
+              }
+            }
+          }
+        }
+      });
   });
 
