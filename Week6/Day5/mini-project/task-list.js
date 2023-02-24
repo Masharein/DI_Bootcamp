1517fc7a6b0ac079fa3de1d8670cbae95b22c949
Masharein <Masharein04061995@gmail.com>
2023-02-24 11:46:11 +0300

    new

--- Week6/Day5/mini-project/task-list.js
+++ Week6/Day5/mini-project/task-list.js
@@ -50,28 +50,35 @@ tasks.forEach((task) => {
     });
 
     const editBtn = li.querySelector('.edit-btn');
-    editBtn.addEventListener('click', function() {
-        const newName = prompt('Enter a new name:', task.name);
-        if (newName) {
-          task.name = newName;
-          const newDescription = prompt('Enter a new description:', task.description);
-          if (newDescription) {
-            task.description = newDescription;
-            const newStart = prompt('Enter a new start date and time (YYYY-MM-DDTHH:MM):', new Date(task.start).toISOString().slice(0, 16));
-            if (newStart) {
-              task.start = new Date(newStart).getTime();
-              const newEnd = prompt('Enter a new end date and time (YYYY-MM-DDTHH:MM):', new Date(task.end).toISOString().slice(0, 16));
-              if (newEnd) {
-                task.end = new Date(newEnd).getTime();
-                localStorage.setItem('tasks', JSON.stringify(tasks));
-                task.name.textContent =  `Name: ${task.name}<br>`
-                task.description.textContent = 'Description: ' + task.description;
-                task.start.textContent =  `Start date and time: ${task.start}<br>`;
-                task.end.textContent = ` End date and time: ${task.end}<br>`;
-              }
-            }
-          }
-        }
-      });
+    editBtn.addEventListener('click', () => {
+      // Open editing interface for the task
+      const nameInput = prompt('Enter a new name for the task:', task.name);
+      const startInput = prompt('Enter a new start date and time for the task (YYYY-MM-DD HH:mm):', task.start);
+      const endInput = prompt('Enter a new end date and time for the task (YYYY-MM-DD HH:mm):', task.end);
+      const descriptionInput = prompt('Enter a new description for the task:', task.description);
+      
+      // Update task object and local storage
+      if (nameInput) {
+        task.name = nameInput;
+      }
+      if (startInput) {
+        task.start = startInput;
+      }
+      if (endInput) {
+        task.end = endInput;
+      }
+      if (descriptionInput) {
+        task.description = descriptionInput;
+      }
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      
+      // Update list item display
+      const end = new Date(task.end);
+      const daysLeft = 
+      Math.ceil((end.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));;
+      li.style.color = task.isCompleted ? 'green' : 'red';
+      li.innerHTML = `<div> <input type="checkbox" ${task.isCompleted ? 'checked' : ''}> Name: ${task.name}<br> Start date and time: ${task.start}<br> End date and time: ${task.end}<br> Days Left: ${daysLeft}<br> <button class="description-btn">See the Description</button> <button class="edit-btn">Edit</button> <button class="delete-btn">Delete</button> </div> <div style="display: none">${task.description}</div>`;
+ 
+});
   });
 
