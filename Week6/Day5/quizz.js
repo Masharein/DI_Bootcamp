b4e9c8186fce55eb2035f1e7d754f8c6a9b053c4
Masharein <Masharein04061995@gmail.com>
2023-02-24 10:35:56 +0300

    new

--- Week6/Day5/quizz.js
+++ Week6/Day5/quizz.js
@@ -368,9 +368,4 @@
 
     
 
-// 29. Use **async await**, and **the fetch API** to POST some data.
-//     * Retrieve the data from a form (you can use new FormData), and send this data to this API : `https://jsonplaceholder.typicode.com/posts`. 
-//     * The server will send you back some dummy data that you will display on the DOM.
-//     * Use the documentation of the API : https://jsonplaceholder.typicode.com/guide/
-//     * Use the fetch API documentation: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#uploading_json_data
-//     * Use the new FormData documentation with Object.fromEntries() : https://gomakethings.com/the-object.fromentries-method-in-vanilla-js/
+
--- Week6/Day5/quizz29.js
+++ Week6/Day5/quizz29.js
@@ -1,3 +1,10 @@
+// 29. Use **async await**, and **the fetch API** to POST some data.
+//     * Retrieve the data from a form (you can use new FormData), and send this data to this API : `https://jsonplaceholder.typicode.com/posts`. 
+//     * The server will send you back some dummy data that you will display on the DOM.
+//     * Use the documentation of the API : https://jsonplaceholder.typicode.com/guide/
+//     * Use the fetch API documentation: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#uploading_json_data
+//     * Use the new FormData documentation with Object.fromEntries() : https://gomakethings.com/the-object.fromentries-method-in-vanilla-js/
+
 // Retrieve the form element and add a submit event listener to it
 const form = document.querySelector('form');
 form.addEventListener('submit', postData);
@@ -16,7 +23,7 @@ async function postData(event) {
     });
 
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      throw new Error(`Bad result`);
     }
 
     const data = await response.json();
@@ -24,8 +31,8 @@ async function postData(event) {
     // Display the response data on the DOM
     const output = document.querySelector('#output');
     const result = Object.entries(data).map(([key, value]) => {
-      return `<p><strong>${key}:</strong> ${value}</p>`;
-    }).join('');
+      return `<p>${key}: ${value}</p>`;
+    });
 
     output.innerHTML = result;
   } catch (error) {
