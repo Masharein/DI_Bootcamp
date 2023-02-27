cdd4576ff003e27cdc6ad9ad5bbd58804f069ef7
Masharein <Masharein04061995@gmail.com>
2023-02-27 14:57:27 +0300

    new

--- Week7/Day2/public/script.js
+++ Week7/Day2/public/script.js
@@ -10,11 +10,37 @@ const getData = () => {
     })
 }
 
-getData()
+// getData()
 
 const showProducts = (arr) => {
     const html = arr.map(item => {
         return `<div>${item.name} ${item.price}</div>`
     })
     document.getElementById('root').innerHTML = html.join('')
+}
+
+const searchWithParametrs = () => {
+    const prod_id = document.getElementById('prod_id').value
+    fetch(`http://localhost:5000/api/product/${prod_id}`)
+    .then(res => res.json())
+    .then(data => {
+        const arr = []
+        arr.push(data)
+        showProducts(arr)
+        console.log(data)
+    })
+    .catch(err => {console.log(err)
+    })
+}
+const searchWithQuery = () => {
+    const prodname = document.getElementById('prod_name').value
+    fetch(`http://localhost:5000/api/search?q=${prodname}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        showProducts(data)
+       
+    })
+    .catch(err => {console.log(err)
+    })
 }
--- Week7/Day2/server.js
+++ Week7/Day2/server.js
@@ -7,10 +7,47 @@ console.log(products)
 const app = express();
 app.use(cors());
 app.use('/', express.static(__dirname + '/public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.listen(5000, () => {
     console.log('run on port')
 })
 
 app.get('/api/product', (req, res) => {
     res.json(products)
+})
+
+app.get('/api/product/:id', (req, res) => {
+    const product_id = req.params.id;
+    const product = products.find(item => {
+        return item.id == product_id
+    })
+    if(!product) {
+        return res.status(404).json({msg:'not found'})
+    }
+    res.json(product)
+})
+
+
+app.get('/api/search/', (req, res) => {
+    const name = req.query.q;
+    const filterProducts = products.filter(item => {
+        return item.name.toLowerCase().includes(name.toLowerCase())
+    })
+    if(filterProducts.length === 0) {
+        return res.status(200).json({msg:'no such product'})
+    }
+    res.json(filterProducts);
+})
+
+app.post('/api/product', (req,res) => {
+    const newProduct = {
+        id: products.length+1,
+        name: req.body.name,
+        price: req.body.price
+    }
+    products.push(newProduct)
+    console.log(products)
+    res.status(200).json(products)
+    // res.sendStatus(200);
 })
