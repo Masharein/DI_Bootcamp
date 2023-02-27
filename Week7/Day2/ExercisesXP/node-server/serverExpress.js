bc34f9d65bd05089a1ed15ca606f66fb7f3a47bc
Masharein <Masharein04061995@gmail.com>
2023-02-27 18:49:00 +0300

    new

--- Week7/Day2/ExercisesXP/node-server/serverExpress.js
+++ Week7/Day2/ExercisesXP/node-server/serverExpress.js
@@ -1,3 +1,11 @@
+// Exercise 3 : Express
+// Instructions
+// Create a server file, name it - serverExpress.js
+// Use express to create a server. Return an HTML line of code (Use only HTML tags, no HTML files), for requests to the root URL (/).
+// Remember to use the GET method for the server route.
+// Your server should run on http://localhost:3000/
+
+
 const express = require('express');
 const cors = require('cors')
 
