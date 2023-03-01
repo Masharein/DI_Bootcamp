2db5787053274be6b2433f137f1d3dd67528c031
Masharein <Masharein04061995@gmail.com>
2023-03-01 07:21:10 +0300

    new

---
+++ Week7/Day3/demo-express/public/script.js
@@ -0,0 +1,45 @@
+const getData = () => {
+  fetch('http://localhost:5000/api/products')
+  .then(res => res.json())
+  .then(data => {
+    showProducts(data);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
+getData();
+
+const showProducts = (arr) => {
+  const html = arr.map(item => {
+    return `<div><h2>${item.name}</h2><h4>${item.price}</h4></div>`
+  })
+  document.getElementById('root').innerHTML = html.join('')
+}
+
+const searchWithParams = () => {
+  const prod_id = document.getElementById('prod_id').value;
+  fetch(`http://localhost:5000/api/products/${prod_id}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+     const arr = []
+     arr.push(data)
+     showProducts(arr)
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
+
+const searchWithQuery = () => {
+  const prod_name = document.getElementById('prod_name').value;
+  fetch(`http://localhost:5000/api/search?q=${prod_name}`)
+  .then(res => res.json())
+  .then(data => {
+     showProducts(data)
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
