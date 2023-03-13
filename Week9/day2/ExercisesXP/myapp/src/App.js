433dcc6a4d9ea4f4ffc166a00fdd4a909c4bdfb3
Masharein <Masharein04061995@gmail.com>
2023-03-13 11:33:33 +0300

    new

---
+++ Week9/day2/ExercisesXP/myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TransactionList from './components/TransactionList';
+
+function App() {
+  return (
+    <div>
+    <h1>Financial Transactions:</h1>
+    <TransactionList />
+    </div>
+  );
+}
+
+export default App;
