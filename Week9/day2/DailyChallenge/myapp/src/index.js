db088c0fed185a5b36b4df96ee22ad26ee02ff7d
Masharein <Masharein04061995@gmail.com>
2023-03-14 10:04:15 +0300

    new

--- Week9/day2/DailyChallenge/myapp/src/index.js
+++ Week9/day2/DailyChallenge/myapp/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore} from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk'
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
-const store = createStore(searchRobots)
+const logger =createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
